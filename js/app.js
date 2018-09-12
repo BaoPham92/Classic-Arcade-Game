611e42f13eb6c4bc7ea1ca06d2cf6d9e0df91292
Cuza <baopham92@yahoo.com>
2018-09-11 21:35:10 -0400

    Converted prototype class to es2015 syntax.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,50 @@
 // GLOBAL DATA
 
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
 
 // Created a randomize function for tile changing. Adds a little bit of dynamic play for later.
 const randomTile = function(num1 = 0, num2 = 4) {
     return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+}
+
+class Enemy {
+    constructor(){
+
+        // Properties for Enemy class.
+
+        // Enemy placement on x,y axis.
+        this.x = 0;
+        this.y = 0;
+
+        // Sprite img for Enemy to render on canvas element.
+        this.sprite = 'images/enemy-bug.png';
+    }
+
+    // Draw method for the image to be displayed.
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
 
 // Temporary Hero class structure in progress.
